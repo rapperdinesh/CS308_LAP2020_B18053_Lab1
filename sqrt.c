82891f8b0dbfa8b33a6957bdc36f7b7f3062de38
rapperdinesh <db.dineshbhagat07@gmail.com>
2020-10-16 22:14:28 +0530

    Added checking valid number condition

--- sqrt.c
+++ sqrt.c
@@ -11,14 +11,23 @@ positive number. */
 
 int main(int argc, char* argv[]) {
 
+
+
 	if (argc != 2) {
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
+
+	int num_less,num_greater;
+	num_less = floor(input);
+	num_greater = ceil(input);
+
+if(num_less == num_greater){
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program. Exiting.\n");
 	return(0);
+}
 
 } // end main
